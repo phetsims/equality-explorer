b0695bcd228308bcdf7f04beb9e6c5d82b21c37a
Chris Malley <cmalley@pixelzoom.com>
2017-06-12 16:29:00 -0600

    design changes to snapshots UI

--- js/common/view/SnapshotsAccordionBox.js
+++ js/common/view/SnapshotsAccordionBox.js
@@ -1,6 +1,5 @@
 // Copyright 2017, University of Colorado Boulder
 
-//TODO SnapshotsAccordionBox needs to be wide enough to support 3 terms on each side of the equation
 /***
  * Accordion box that allows the student to save and restore snapshots, specific configurations of a scene.
  *
@@ -14,11 +13,10 @@ define( function( require ) {
   var equalityExplorer = require( 'EQUALITY_EXPLORER/equalityExplorer' );
   var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
   var HBox = require( 'SCENERY/nodes/HBox' );
+  var HSeparator = require( 'SUN/HSeparator' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
   var PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
   var Text = require( 'SCENERY/nodes/Text' );
   var VBox = require( 'SCENERY/nodes/VBox' );
@@ -27,7 +25,6 @@ define( function( require ) {
   var mySnapshotsString = require( 'string!EQUALITY_EXPLORER/mySnapshots' );
 
   // constants
-  var NUMBER_FONT = new PhetFont( 16 );
   var BUTTON_X_MARGIN = 8;
   var BUTTON_Y_MARGIN = 5;
 
@@ -57,87 +54,82 @@ define( function( require ) {
 
     var vBoxChildren = [];
 
+    vBoxChildren.push( new HSeparator( 225, {
+      stroke: 'rgb( 200, 200, 200 )'
+    } ) );
+
+    // button icons
     var snapshotIcon = new FontAwesomeNode( 'camera', { scale: 0.3 } );
-    var trashIcon = new FontAwesomeNode( 'trash', { scale: 0.3 } );
-    var maxIconWidth = Math.max( snapshotIcon.width, trashIcon.width );
-    var maxIconHeight = Math.max( snapshotIcon.height, trashIcon.height );
+    var loadIcon = new FontAwesomeNode( 'reorder', { scale: 0.3 } );
+    var trashIcon = new FontAwesomeNode( 'trash', { scale: 0.3 } ); //TODO left arrow icon
+
+    // for making all buttons the same size
+    var maxIconWidth = _.maxBy( [ snapshotIcon, loadIcon, trashIcon], function( icon ) { return icon.width; } ).width;
+    var maxIconHeight = _.maxBy( [ snapshotIcon, loadIcon, trashIcon], function( icon ) { return icon.height; } ).height;
 
     // Create a row for each snapshot
     for ( var i = 0; i < options.numberOfSnapshots; i++ ) {
 
-      var numberLabel = new Text( i + 1, {
-        font: NUMBER_FONT
-      } );
-
-      var numberButton = new RectangularPushButton( {
-        content: new Text( i + 1, {
-          font: NUMBER_FONT,
-          xMargin: 5,
-          yMargin: 2
-        } ),
-        baseColor: PhetColorScheme.PHET_LOGO_YELLOW,
-        touchAreaXDilation: 5,
-        touchAreaYDilation: 5,
-        center: numberLabel.center,
-        visible: false
-      } );
-
-      var numbersParent = new Node( {
-        children: [ numberLabel, numberButton ]
-      } );
-
-      var equationNode = new Rectangle( 0, 0, 160, 50 ); //TODO use EquationNode
-
-      var snapshotButton = new RectangularPushButton( {
-        content: snapshotIcon,
-        baseColor: PhetColorScheme.PHET_LOGO_YELLOW,
-        xMargin: BUTTON_X_MARGIN + ( maxIconWidth - snapshotIcon.width ),
-        yMargin: BUTTON_Y_MARGIN + ( maxIconHeight - snapshotIcon.height ),
-        touchAreaXDilation: 5,
-        touchAreaYDilation: 5
-      } );
-
-      var trashButton = new RectangularPushButton( {
-        content: trashIcon,
-        center: snapshotButton.center,
-        baseColor: 'white',
-        xMargin: BUTTON_X_MARGIN + ( maxIconWidth - trashIcon.width ),
-        yMargin: BUTTON_Y_MARGIN + ( maxIconHeight - trashIcon.height ),
-        touchAreaXDilation: 5,
-        touchAreaYDilation: 5,
-        visible: false
-      } );
-
-      var buttonsParent = new Node( {
-        children: [ trashButton, snapshotButton ]
-      } );
-
-      //TODO placeholder that shows worst case snapshot, 3 terms on each side
       if ( i === 0 ) {
-        numberButton.visible = true;
-        snapshotButton.visible = false;
-        trashButton.visible = true;
-        var equationText = new Text( '-40x + 40 = -40x + 40', {
-          font: new PhetFont( 30 ),
-          center: equationNode.center,
-          maxWidth: equationNode.width
+        var equationText = new Text( '-40 X + 40 X + 40 X = -40 X + 40 X + 40 X', {
+          font: new PhetFont( 12 )
         } );
-        equationNode.addChild( equationText );
+        vBoxChildren.push( equationText );
       }
+      else {
 
-      var hBox = new HBox( {
-        spacing: 10,
-        children: [ numbersParent, equationNode, buttonsParent ]
-      } );
+        var snapshotButton = new RectangularPushButton( {
+          content: snapshotIcon,
+          baseColor: PhetColorScheme.PHET_LOGO_YELLOW,
+          xMargin: BUTTON_X_MARGIN + ( maxIconWidth - snapshotIcon.width ),
+          yMargin: BUTTON_Y_MARGIN + ( maxIconHeight - snapshotIcon.height ),
+          touchAreaXDilation: 5,
+          touchAreaYDilation: 5
+        } );
 
-      vBoxChildren.push( hBox );
+        vBoxChildren.push( snapshotButton );
+      }
     }
 
-    var contentNode = new VBox( {
+    vBoxChildren.push( new HSeparator( 225, {
+      stroke: 'rgb( 200, 200, 200 )'
+    } ) );
+
+    var snapshotsVBox = new VBox( {
+      spacing: 30,
       children: vBoxChildren
     } );
 
-    AccordionBox.call( this, contentNode, options );
+    var loadButton = new RectangularPushButton( {
+      content: loadIcon,
+      baseColor: 'white',
+      xMargin: BUTTON_X_MARGIN + ( maxIconWidth - loadIcon.width ),
+      yMargin: BUTTON_Y_MARGIN + ( maxIconHeight - loadIcon.height ),
+      touchAreaXDilation: 5,
+      touchAreaYDilation: 5
+    } );
+
+    var trashButton = new RectangularPushButton( {
+      content: trashIcon,
+      baseColor: 'white',
+      xMargin: BUTTON_X_MARGIN + ( maxIconWidth - trashIcon.width ),
+      yMargin: BUTTON_Y_MARGIN + ( maxIconHeight - trashIcon.height ),
+      touchAreaXDilation: 5,
+      touchAreaYDilation: 5
+    } );
+
+    var buttonGroup = new HBox( {
+      spacing: 20,
+      children: [ loadButton, trashButton ]
+    });
+    vBoxChildren.push( buttonGroup );
+    
+    var contentVBox = new VBox( {
+      spacing: 10,
+      children: [ snapshotsVBox, buttonGroup ]
+    } );
+
+    AccordionBox.call( this, contentVBox, options );
   }
 
   equalityExplorer.register( 'SnapshotsAccordionBox', SnapshotsAccordionBox );
