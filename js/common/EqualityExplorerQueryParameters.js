d372487581f250490b5b6b5009ce87e331385dc5
Chris Malley <cmalley@pixelzoom.com>
2017-06-27 16:45:25 -0600

    add logging

--- js/common/EqualityExplorerQueryParameters.js
+++ js/common/EqualityExplorerQueryParameters.js
@@ -16,14 +16,22 @@ define( function( require ) {
     // enables console logging
     log: { type: 'flag' },
 
-    // populates the output carousel with 1 card of each type
-    populateOutput: { type: 'flag' },
-
     // makes all animation run slowly, so that things are easier to grab while they're animating
     slow: { type: 'flag' }
   } );
 
   equalityExplorer.register( 'EqualityExplorerQueryParameters', EqualityExplorerQueryParameters );
+
+  // enable logging to the console
+  if ( EqualityExplorerQueryParameters.log ) {
+
+    console.log( 'enabling log' );
+    equalityExplorer.log = function( message ) {
+      console.log( '%clog: ' + message, 'color: #009900' ); // display messages in green
+    };
+
+    equalityExplorer.log( 'slow=' + EqualityExplorerQueryParameters.slow );
+  }
 
   return EqualityExplorerQueryParameters;
 } );
