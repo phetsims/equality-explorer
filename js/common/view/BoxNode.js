62c7f06860c05b0cfae59963712bd1bdf1e9a047
Chris Malley <cmalley@pixelzoom.com>
2017-09-11 13:21:30 -0600

    doc

--- js/common/view/BoxNode.js
+++ js/common/view/BoxNode.js
@@ -2,7 +2,7 @@
 
 //TODO migrate to common code?
 /**
- * A pseudo-3D box, with perspective.
+ * A pseudo-3D box, with perspective. The front and top faces are visible.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
