888a1ee357d1dbed969b0a418f8ac876d5ac2eca
Chris Malley <cmalley@pixelzoom.com>
2017-09-29 15:49:17 -0600

    revised organize icon

--- js/common/view/OrganizeButton.js
+++ js/common/view/OrganizeButton.js
@@ -29,27 +29,15 @@ define( function( require ) {
 
       // supertype options
       baseColor: PhetColorScheme.BUTTON_YELLOW,
+      xMargin: 8,
+      yMargin: 4,
       touchAreaXDilation: 5,
       touchAreaYDilation: 5
-      
+
     }, options );
 
-    //TODO finalize icon for OrganizeButton, see https://github.com/phetsims/equality-explorer/issues/9
-    var diameter = 7;
-    var spacing = 3;
-    var iconShape = new Shape()
-      .rect( 0, 0, diameter, diameter )
-      .rect( 0, diameter + spacing, diameter, diameter )
-      .rect( diameter + spacing, diameter + spacing, diameter, diameter )
-      .circle( 3 * ( diameter + spacing ), 0.5 * diameter, diameter / 2 )
-      .circle( 3 * ( diameter + spacing ), 1.5 * diameter + spacing, diameter / 2 );
-
-    var iconNode = new Path( iconShape, {
-      fill: 'black'
-    } );
-
     assert && assert( !options.content, 'this subtype defines its content' );
-    options.content = iconNode;
+    options.content = createButtonContent();
 
     assert && assert( !options.listener, 'this subtype defines its listener' );
     options.listener = function() {
@@ -61,6 +49,57 @@ define( function( require ) {
   }
 
   equalityExplorer.register( 'OrganizeButton', OrganizeButton );
+
+  /**
+   * Creates the icon that appears on the button.
+   * @returns {Node}
+   */
+  function createButtonContent() {
+
+    var diameter = 6; // diameter of each item
+    var xSpacing = 2; // horizontal space between columns
+    var xSpacingExtra = 3; // extra horizontal space between columns with different types of items
+    var ySpacing = 1; // vertical space between items in a column
+
+    var iconShape = new Shape();
+
+    // 2 columns of 3 circles
+    var left = 0;
+    var top = 0;
+    for ( var column = 0; column < 2; column++ ) {
+      for (var row = 0; row < 3; row++ ) {
+        iconShape.circle( left + 0.5 * diameter, top + 0.5 * diameter, diameter / 2 );
+        top += ( diameter + ySpacing );
+      }
+      left += ( diameter + xSpacing );
+      top = 0;
+    }
+
+    // 1 column of 2 squares
+    left += xSpacingExtra;
+    top = diameter + ySpacing;
+    for ( row = 0; row < 2; row++ ) {
+      iconShape.rect( left, top, diameter, diameter );
+      top += ( diameter + ySpacing );
+    }
+
+    // 1 column of 3 triangles
+    left += ( diameter + xSpacing + xSpacingExtra );
+    top = 0;
+    for ( row = 0; row < 3; row++ ) {
+      
+      // described from top center, moving clockwise
+      iconShape.moveTo( left + 0.5 * diameter, top )
+        .lineTo( left + diameter, top + diameter )
+        .lineTo( left, top + diameter )
+        .close();
+      top += ( diameter + ySpacing );
+    }
+
+    return new Path( iconShape, {
+      fill: 'black'
+    } );
+  }
 
   return inherit( RectangularPushButton, OrganizeButton );
 } );
