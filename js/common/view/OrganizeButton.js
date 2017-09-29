62d1665741408e6c7c4df072b5b46526cc04e12c
Chris Malley <cmalley@pixelzoom.com>
2017-09-29 15:57:57 -0600

    update

--- js/common/view/OrganizeButton.js
+++ js/common/view/OrganizeButton.js
@@ -50,7 +50,7 @@ define( function( require ) {
 
   equalityExplorer.register( 'OrganizeButton', OrganizeButton );
 
-  //TODO #9 this ugly piece of code should be replaced with a PNG file
+  //TODO #9 this ugly piece of code could be replaced with a PNG file
   /**
    * Creates the icon that appears on the button.
    * @returns {Node}
