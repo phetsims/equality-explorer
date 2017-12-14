944cf335ad949bd2bb1c487a2ad0616a46f9e128
Chris Malley <cmalley@pixelzoom.com>
2017-12-14 14:50:32 -0700

    clean up options

--- js/common/view/ItemIcons.js
+++ js/common/view/ItemIcons.js
@@ -47,72 +47,68 @@ define( function( require ) {
 
   // constants
   var ITEM_HEIGHT = 32;
-  var IMAGE_OPTIONS = { maxHeight: ITEM_HEIGHT };
+  var COMMON_OPTIONS = { maxHeight: ITEM_HEIGHT };
 
   var ItemIcons = {
 
-    APPLE_NODE: new Image( appleImage, IMAGE_OPTIONS ),
-    APPLE_SHADOW_NODE: new Image( appleShadowImage, IMAGE_OPTIONS ),
+    APPLE_NODE: new Image( appleImage, COMMON_OPTIONS ),
+    APPLE_SHADOW_NODE: new Image( appleShadowImage, COMMON_OPTIONS ),
 
-    CAT_NODE: new Image( catImage, IMAGE_OPTIONS ),
-    CAT_SHADOW_NODE: new Image( catShadowImage, IMAGE_OPTIONS ),
+    CAT_NODE: new Image( catImage, COMMON_OPTIONS ),
+    CAT_SHADOW_NODE: new Image( catShadowImage, COMMON_OPTIONS ),
 
-    COIN1_NODE: new Image( coin1Image, IMAGE_OPTIONS ),
-    COIN1_SHADOW_NODE: new Image( coin1ShadowImage, IMAGE_OPTIONS ),
+    COIN1_NODE: new Image( coin1Image, COMMON_OPTIONS ),
+    COIN1_SHADOW_NODE: new Image( coin1ShadowImage, COMMON_OPTIONS ),
 
-    COIN2_NODE: new Image( coin2Image, IMAGE_OPTIONS ),
-    COIN2_SHADOW_NODE: new Image( coin2ShadowImage, IMAGE_OPTIONS ),
+    COIN2_NODE: new Image( coin2Image, COMMON_OPTIONS ),
+    COIN2_SHADOW_NODE: new Image( coin2ShadowImage, COMMON_OPTIONS ),
 
-    COIN3_NODE: new Image( coin3Image, IMAGE_OPTIONS ),
-    COIN3_SHADOW_NODE: new Image( coin3ShadowImage, IMAGE_OPTIONS ),
+    COIN3_NODE: new Image( coin3Image, COMMON_OPTIONS ),
+    COIN3_SHADOW_NODE: new Image( coin3ShadowImage, COMMON_OPTIONS ),
 
-    DOG_NODE: new Image( dogImage, IMAGE_OPTIONS ),
-    DOG_SHADOW_NODE: new Image( dogShadowImage, IMAGE_OPTIONS ),
+    DOG_NODE: new Image( dogImage, COMMON_OPTIONS ),
+    DOG_SHADOW_NODE: new Image( dogShadowImage, COMMON_OPTIONS ),
 
-    LEMON_NODE: new Image( lemonImage, IMAGE_OPTIONS ),
-    LEMON_SHADOW_NODE: new Image( lemonShadowImage, IMAGE_OPTIONS ),
+    LEMON_NODE: new Image( lemonImage, COMMON_OPTIONS ),
+    LEMON_SHADOW_NODE: new Image( lemonShadowImage, COMMON_OPTIONS ),
 
-    ORANGE_NODE: new Image( orangeImage, IMAGE_OPTIONS ),
-    ORANGE_SHADOW_NODE: new Image( orangeShadowImage, IMAGE_OPTIONS ),
+    ORANGE_NODE: new Image( orangeImage, COMMON_OPTIONS ),
+    ORANGE_SHADOW_NODE: new Image( orangeShadowImage, COMMON_OPTIONS ),
 
-    SPHERE_NODE: new Image( sphereImage, IMAGE_OPTIONS ),
-    SPHERE_SHADOW_NODE: new Image( sphereShadowImage, IMAGE_OPTIONS ),
+    SPHERE_NODE: new Image( sphereImage, COMMON_OPTIONS ),
+    SPHERE_SHADOW_NODE: new Image( sphereShadowImage, COMMON_OPTIONS ),
 
-    SQUARE_NODE: new Image( squareImage, IMAGE_OPTIONS ),
-    SQUARE_SHADOW_NODE: new Image( squareShadowImage, IMAGE_OPTIONS ),
+    SQUARE_NODE: new Image( squareImage, COMMON_OPTIONS ),
+    SQUARE_SHADOW_NODE: new Image( squareShadowImage, COMMON_OPTIONS ),
 
-    TURTLE_NODE: new Image( turtleImage, IMAGE_OPTIONS ),
-    TURTLE_SHADOW_NODE: new Image( turtleShadowImage, IMAGE_OPTIONS ),
+    TURTLE_NODE: new Image( turtleImage, COMMON_OPTIONS ),
+    TURTLE_SHADOW_NODE: new Image( turtleShadowImage, COMMON_OPTIONS ),
 
-    POSITIVE_ONE_NODE: new IntegerNode( 1, {
+    // 1, -1 and their shadow
+    POSITIVE_ONE_NODE: new IntegerNode( 1, _.extend( {}, {
       radius: ITEM_HEIGHT / 2,
-      fill: 'rgb( 246, 228, 213 )',
-      maxHeight: ITEM_HEIGHT
-    } ),
-    NEGATIVE_ONE_NODE: new IntegerNode( -1, {
+      fill: 'rgb( 246, 228, 213 )'
+    }, COMMON_OPTIONS ) ),
+    NEGATIVE_ONE_NODE: new IntegerNode( -1, _.extend( {}, {
       radius: ITEM_HEIGHT / 2,
       fill: 'rgb( 248, 238, 229 )',
-      lineDash: [ 3, 3 ],
-      maxHeight: ITEM_HEIGHT
-    } ),
-    ONE_SHADOW_NODE: new Circle( ITEM_HEIGHT / 2, {
-      fill: 'black',
-      maxHeight: ITEM_HEIGHT
-    } ),
+      lineDash: [ 3, 3 ]
+    }, COMMON_OPTIONS ) ),
+    ONE_SHADOW_NODE: new Circle( ITEM_HEIGHT / 2, _.extend( {}, {
+      fill: 'black'
+    }, COMMON_OPTIONS ) ),
 
-    POSITIVE_X_NODE: new VariableNode( xString, {
-      fill: 'rgb( 49, 193, 238 )',
-      maxHeight: ITEM_HEIGHT
-    } ),
-    NEGATIVE_X_NODE: new VariableNode( '-' + xString, {
+    // x, -x and their shadow
+    POSITIVE_X_NODE: new VariableNode( xString, _.extend( {}, {
+      fill: 'rgb( 49, 193, 238 )'
+    }, COMMON_OPTIONS ) ),
+    NEGATIVE_X_NODE: new VariableNode( '-' + xString, _.extend( {}, {
       fill: 'rgb( 99, 212, 238 )',
-      lineDash: [ 4, 4 ],
-      maxHeight: ITEM_HEIGHT
-    } ),
-    X_SHADOW_NODE: new Rectangle( 0, 0, ITEM_HEIGHT, ITEM_HEIGHT, {
-      fill: 'black',
-      maxHeight: ITEM_HEIGHT
-    } )
+      lineDash: [ 4, 4 ]
+    }, COMMON_OPTIONS ) ),
+    X_SHADOW_NODE: new Rectangle( 0, 0, ITEM_HEIGHT, ITEM_HEIGHT, _.extend( {}, {
+      fill: 'black'
+    }, COMMON_OPTIONS ) )
   };
 
   equalityExplorer.register( 'ItemIcons', ItemIcons );
