0f1d486dba3eb486cbbd3ba84cfe30c30ca65ff5
Chris Malley <cmalley@pixelzoom.com>
2017-12-18 14:51:50 -0700

    doc

--- js/common/view/XCheckBox.js
+++ js/common/view/XCheckBox.js
@@ -1,7 +1,7 @@
 // Copyright 2017, University of Colorado Boulder
 
 /**
- * Check box used to show the value of the variable (x) in the Snapshots accordion box.
+ * Check box used to show/hide the value of the variable 'x' in the Snapshots accordion box.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
