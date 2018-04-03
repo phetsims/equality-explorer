6348af9ae517241303e526a95e0c3c7fe13565d3
Chris Malley <cmalley@pixelzoom.com>
2018-04-03 17:28:08 -0600

    add Next button, have it increment score for now

--- js/solveit/view/PlayNode.js
+++ js/solveit/view/PlayNode.js
@@ -12,13 +12,20 @@ define( function( require ) {
   var equalityExplorer = require( 'EQUALITY_EXPLORER/equalityExplorer' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
+  var PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
   var RichText = require( 'SCENERY/nodes/RichText' );
   var ScoreDisplayNumberAndStar = require( 'VEGAS/ScoreDisplayNumberAndStar' );
   var StatusBar = require( 'VEGAS/StatusBar' );
+  var Text = require( 'SCENERY/nodes/Text' );
+
+  // strings
+  var nextString = require( 'string!EQUALITY_EXPLORER/next' );
 
   // constants
   var LEVEL_FONT = new PhetFont( 20 );
+  var NEXT_BUTTON_FONT = new PhetFont( 16 );
 
   /**
    * @param {SolveItModel} model
@@ -48,8 +55,18 @@ define( function( require ) {
       backButtonListener: options.backButtonListener
     } );
 
+    var nextButton = new RectangularPushButton( {
+      content: new Text( nextString, { font: NEXT_BUTTON_FONT } ),
+      baseColor: PhetColorScheme.PHET_LOGO_YELLOW,
+      centerX: statusBar.centerX,
+      top: statusBar.bottom + 30,
+      listener: function() {
+        scoreProperty.value++;
+      }
+    } );
+
     assert && assert( !options.children, 'PlayNode sets children' );
-    options.children = [ statusBar ];
+    options.children = [ statusBar, nextButton ];
 
     Node.call( this, options );
 
