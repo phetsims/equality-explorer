87d9e52a01eaa7d78286f9354e4e1ad861e8c846
Chris Malley <cmalley@pixelzoom.com>
2018-04-09 14:13:15 -0600

    delete left* and right* query params for populating scale #8
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/EqualityExplorerQueryParameters.js
+++ js/common/EqualityExplorerQueryParameters.js
@@ -19,7 +19,6 @@ define( function( require ) {
 
   // modules
   var equalityExplorer = require( 'EQUALITY_EXPLORER/equalityExplorer' );
-  var Util = require( 'DOT/Util' );
 
   var EqualityExplorerQueryParameters = QueryStringMachine.getAll( {
 
@@ -79,105 +78,10 @@ define( function( require ) {
 
     // Shows answers to challenges in the 'Solve It!' screen
     // For internal use only, not public facing.
-    showAnswers: { type: 'flag' },
-
-    // Number of terms that are initially on the left plate in the Basics screen.
-    // This is intended to be used for debugging and testing, not in production situations.
-    // Example: ?leftBasics=10,11,12
-    // See https://github.com/phetsims/equality-explorer/issues/8
-    // For internal use only, not public facing.
-    leftBasics: {
-      type: 'array',
-      elementSchema: {
-        type: 'number'
-      },
-      defaultValue: [ 0, 0, 0 ], // in the order that terms appear in the toolbox below the scale
-      isValidValue: function( value ) {
-        return isValidTermsArray( value, 3 );
-      }
-    },
-
-    // Similar to leftBasics, but for the right plate in the Basics screen.
-    // For internal use only, not public facing.
-    rightBasics: {
-      type: 'array',
-      elementSchema: {
-        type: 'number'
-      },
-      defaultValue: [ 0, 0, 0 ],
-      isValidValue: function( value ) {
-        return isValidTermsArray( value, 3 );
-      }
-    },
-
-    // Similar to leftBasics, but for the Numbers screen.
-    // For internal use only, not public facing.
-    leftNumbers: {
-      type: 'array',
-      elementSchema: {
-        type: 'number'
-      },
-      defaultValue: [ 0, 0 ], // in the order that terms appear in the toolbox below the scale
-      isValidValue: function( value ) {
-        return isValidTermsArray( value, 2 );
-      }
-    },
-
-    // Similar to rightBasics, but for the Numbers screen.
-    // For internal use only, not public facing.
-    rightNumbers: {
-      type: 'array',
-      elementSchema: {
-        type: 'number'
-      },
-      defaultValue: [ 0, 0 ],
-      isValidValue: function( value ) {
-        return isValidTermsArray( value, 2 );
-      }
-    },
-
-    // Similar to leftBasics, but for the Variables screen.
-    // For internal use only, not public facing.
-    leftVariables: {
-      type: 'array',
-      elementSchema: {
-        type: 'number'
-      },
-      defaultValue: [ 0, 0, 0, 0 ], // in the order that terms appear in the toolbox below the scale
-      isValidValue: function( value ) {
-        return isValidTermsArray( value, 4 );
-      }
-    },
-
-    // Similar to rightBasics, but for the Variables screen.
-    // For internal use only, not public facing.
-    rightVariables: {
-      type: 'array',
-      elementSchema: {
-        type: 'number'
-      },
-      defaultValue: [ 0, 0, 0, 0 ],
-      isValidValue: function( value ) {
-        return isValidTermsArray( value, 4 );
-      }
-    }
+    showAnswers: { type: 'flag' }
   } );
 
   equalityExplorer.register( 'EqualityExplorerQueryParameters', EqualityExplorerQueryParameters );
-
-  /**
-   * Validates an array that indicates the number of terms on a plate.
-   * @param {[]} array
-   * @param {number} length - required length of array
-   * @returns {boolean}
-   */
-  function isValidTermsArray( array, length ) {
-    return ( array.length === length ) &&
-           // every value in the array is an integer
-           ( _.every( array, function( value ) { return value >= 0 && Util.isInteger( value ); } ) ) &&
-           // sum of values in the array doesn't exceed number of cells in grid
-           ( _.reduce( array, function( sum, n ) { return sum + n; } ) <= 36 );
-  }
 
   // log the values of all sim-specific query parameters
   phet.log && phet.log( 'query parameters: ' + JSON.stringify( EqualityExplorerQueryParameters, null, 2 ) );
