b643b55229fa8a4832bcd96680b54d1c621bc9e5
Chris Malley <cmalley@pixelzoom.com>
2018-04-16 16:47:51 -0600

    doc
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/solveit/model/ChallengeGenerator.js
+++ js/solveit/model/ChallengeGenerator.js
@@ -2,6 +2,7 @@
 
 /**
  * Abstract base type for challenge generators.
+ * See specification in https://docs.google.com/document/d/1vG5U9HhcqVGMvmGGXry28PLqlNWj25lStDP2vSWgUOo/edit.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
@@ -25,7 +26,11 @@ define( function( require ) {
     // @public (read-only) number of challenges generated
     this.numberOfChallenges = 0;
 
-    // @protected value of x for the previous challenge, so we don't use the same value for consecutive challenges
+    // @protected value of x for the previous challenge, so we don't use the same value for consecutive challenges.
+    // The design document says "It’s OK to generate the same coefficient or constant for consecutive challenges.
+    // Do not generate the exact same challenge (coefficient, constant, AND value for x) twice in a row." So we
+    // only need to pick one quantity that is not the same, and we have chosen 'x', since it's common to all
+    // challenges.
     this.xPrevious = 0;
 
     // @protected {Random}
