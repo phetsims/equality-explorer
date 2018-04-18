eeade0e18ca1efeb0b6db50d7c8b9c2b13e7c29e
Chris Malley <cmalley@pixelzoom.com>
2018-04-18 11:09:26 -0600

    TODO
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/solveit/view/SolveItSceneNode.js
+++ js/solveit/view/SolveItSceneNode.js
@@ -114,6 +114,7 @@ define( function( require ) {
     // Layer when universal operation animation occurs
     var operationAnimationLayer = new Node();
 
+    //TODO clicking in top/bottom areas of value is not working, was working in 1.0.0-dev.92
     // Universal Operation control
     var universalOperationControl = new UniversalOperationControl( scene, operationAnimationLayer, {
       centerX: scene.scale.location.x, // centered on the scale
