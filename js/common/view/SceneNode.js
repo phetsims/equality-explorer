6e11ef2149a0ca9004af1bca56d3dcf62418188c
Chris Malley <cmalley@pixelzoom.com>
2018-04-26 22:54:51 -0600

    add TermNodes in front, not in back
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/SceneNode.js
+++ js/common/view/SceneNode.js
@@ -44,7 +44,7 @@ define( function( require ) {
 
       // create a TermNode
       var termNode = termCreator.createTermNode( term );
-      termsLayer.insertChild( 0, termNode ); // behind other Nodes
+      termsLayer.addChild( termNode );
 
       // Clean up when the term is disposed. Term.dispose handles removal of this listener.
       term.disposedEmitter.addListener( function( term ) {
