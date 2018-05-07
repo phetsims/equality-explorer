1080014f43ec2f7de2d1734cac20fc64c9644c9d
Chris Malley <cmalley@pixelzoom.com>
2018-05-07 13:05:36 -0600

    https://github.com/phetsims/equality-explorer-basics/issues/3
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/basics/model/ObjectType.js
+++ js/basics/model/ObjectType.js
@@ -13,16 +13,25 @@ define( function( require ) {
   var equalityExplorer = require( 'EQUALITY_EXPLORER/equalityExplorer' );
   var inherit = require( 'PHET_CORE/inherit' );
   var NumberProperty = require( 'AXON/NumberProperty' );
+  var Range = require( 'DOT/Range' );
   var Util = require( 'DOT/Util' );
+
+  // constants
+  var DEFAULT_WEIGHT_RANGE = new Range( 1, 10 );
 
   /**
    * @param {string} debugName - internal name for the object type, not visible to the user
    * @param {HTMLImageElement} image - image that represents the object
    * @param {HTMLImageElement} shadow - shadow shown while dragging
    * @param {number} weight - initial integer weight of 1 object
+   * @param {Object} [options]
    * @constructor
    */
-  function ObjectType( debugName, image, shadow, weight ) {
+  function ObjectType( debugName, image, shadow, weight, options ) {
+
+    options = _.extend( {
+      weightRange: DEFAULT_WEIGHT_RANGE
+    }, options );
 
     assert && assert( Util.isInteger( weight ), 'weight must be an integer: ' + weight );
 
@@ -30,6 +39,7 @@ define( function( require ) {
     this.debugName = debugName;
     this.image = image;
     this.shadow = shadow;
+    this.weightRange = options.weightRange;
 
     // @public
     this.weightProperty = new NumberProperty( weight, {
