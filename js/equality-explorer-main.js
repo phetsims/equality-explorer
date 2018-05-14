cadc7bb2f786d183fa78d0a89b9b90f7803fb471
Chris Malley <cmalley@pixelzoom.com>
2018-05-14 12:25:26 -0600

    revert previous commit
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/equality-explorer-main.js
+++ js/equality-explorer-main.js
@@ -9,9 +9,15 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var BasicsScreen = require( 'EQUALITY_EXPLORER/basics/BasicsScreen' );
   var EqualityExplorerConstants = require( 'EQUALITY_EXPLORER/common/EqualityExplorerConstants' );
+  var EqualityExplorerQueryParameters = require( 'EQUALITY_EXPLORER/common/EqualityExplorerQueryParameters' );
+  var NumbersScreen = require( 'EQUALITY_EXPLORER/numbers/NumbersScreen' );
+  var OperationsScreen = require( 'EQUALITY_EXPLORER/operations/OperationsScreen' );
   var Sim = require( 'JOIST/Sim' );
   var SimLauncher = require( 'JOIST/SimLauncher' );
+  var SolveItScreen = require( 'EQUALITY_EXPLORER/solveit/SolveItScreen' );
+  var VariablesScreen = require( 'EQUALITY_EXPLORER/variables/VariablesScreen' );
   var XYScreen = require( 'EQUALITY_EXPLORER/xy/XYScreen' );
 
   // strings
@@ -19,7 +25,21 @@ define( function( require ) {
 
   SimLauncher.launch( function() {
 
-    var sim = new Sim( equalityExplorerTitleString, [ new XYScreen() ], {
+    // production screens
+    var screens = [
+      new BasicsScreen(),
+      new NumbersScreen(),
+      new VariablesScreen(),
+      new OperationsScreen(),
+      new SolveItScreen()
+    ];
+
+    // non-production screen for testing multi-variable support
+    if ( EqualityExplorerQueryParameters.xy ) {
+      screens.push( new XYScreen() );
+    }
+
+    var sim = new Sim( equalityExplorerTitleString, screens, {
       credits: EqualityExplorerConstants.CREDITS
     } );
     sim.start();
