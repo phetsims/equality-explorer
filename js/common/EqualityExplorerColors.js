8f8553f41aa1a5b14023b19ff3ff696674ddb37d
Chris Malley <cmalley@pixelzoom.com>
2018-05-24 12:44:32 -0600

    revert stroke around unselected snapshots #112
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/EqualityExplorerColors.js
+++ js/common/EqualityExplorerColors.js
@@ -23,8 +23,7 @@ define( function( require ) {
 
     // snapshots
     SNAPSHOT_SELECTED_STROKE: 'rgb( 85, 169, 223 )',
-    SNAPSHOT_DESELECTED_STROKE: 'rgba( 0, 0, 0, 0.05 )',
-    // SNAPSHOT_DESELECTED_STROKE: 'transparent',
+    SNAPSHOT_DESELECTED_STROKE: 'transparent',
 
     // terms
     POSITIVE_CONSTANT_FILL: 'rgb( 246, 228, 213 )', // beige
