c24756489121c3c186b3e17ba3cc48a616b252d0
Chris Malley <cmalley@pixelzoom.com>
2018-05-29 16:16:46 -0600

    doc
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/basics/model/ObjectVariable.js
+++ js/basics/model/ObjectVariable.js
@@ -1,7 +1,7 @@
 // Copyright 2018, University of Colorado Boulder
 
 /**
- * Describes a variable associated with a type of real-world object (sphere, apple, dog, turtle,..)
+ * Describes a variable associated with a type of real-world object (sphere, apple, coin, dog, ...)
  * This is a specialization of Variable (which is a symbolic variable, e.g. 'x') that carries additional
  * information related to the real-world object.
  *
