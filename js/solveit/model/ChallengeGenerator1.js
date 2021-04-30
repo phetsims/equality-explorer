e492cc2b739ac0ad07fb40740b1cc1ef4151b5a4
pixelzoom <cmalley@pixelzoom.com>
2021-04-29 18:01:39 -0600

    revise levels 1 & 2, https://github.com/phetsims/equality-explorer/issues/164

--- js/solveit/model/ChallengeGenerator1.js
+++ js/solveit/model/ChallengeGenerator1.js
@@ -8,44 +8,57 @@
  */
 
 import dotRandom from '../../../../dot/js/dotRandom.js';
+import merge from '../../../../phet-core/js/merge.js';
 import Fraction from '../../../../phetcommon/js/model/Fraction.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
-import EqualityExplorerQueryParameters from '../../common/EqualityExplorerQueryParameters.js';
 import equalityExplorer from '../../equalityExplorer.js';
 import equalityExplorerStrings from '../../equalityExplorerStrings.js';
 import Challenge from './Challenge.js';
 import ChallengeGenerator from './ChallengeGenerator.js';
 
 // strings (debug)
-const PATTERN1 = 'level 1, type 1, ax = c<br>' +
+const PATTERN1 = 'level {{level}}, type 1, ax = c<br>' +
                  'x = {{x}}<br>' +
                  'a = {{a}}<br>' +
                  'c = a * x = {{c}}';
-const PATTERN2 = 'level 1, type 2, x + b = c<br>' +
+const PATTERN2 = 'level {{level}}, type 2, x + b = c<br>' +
                  'x = {{x}}<br>' +
                  'b = {{b}}<br>' +
                  'c = x + b = {{c}}';
-const PATTERN3 = 'level 1, type 3, x/d = c<br>' +
+const PATTERN3 = 'level {{level}}, type 3, x/d = c<br>' +
                  'c = {{c}}<br>' +
                  'd = {{d}}<br>' +
                  'x = c * d = {{x}} ';
 
 // constants
-const X_VALUES = ChallengeGenerator.rangeToArray( -40, 40 );
-const A_VALUES = EqualityExplorerQueryParameters.easyLevel1 ?
-                 ChallengeGenerator.rangeToArray( 2, 10 ) :
-                 ChallengeGenerator.rangeToArray( -10, 10 );
-const B_VALUES = ChallengeGenerator.rangeToArray( -10, 10 );
-const C_VALUES = ChallengeGenerator.rangeToArray( -10, 10 );
-const D_VALUES = EqualityExplorerQueryParameters.easyLevel1 ?
-                 ChallengeGenerator.rangeToArray( 2, 10 ) :
-                 ChallengeGenerator.rangeToArray( -10, 10 );
 const MAX_ATTEMPTS = 50; // max attempts in a while loop
 
 class ChallengeGenerator1 extends ChallengeGenerator {
 
-  constructor() {
+  constructor( options ) {
+
+    options = merge( {
+
+      // These sets of values are different for Level 2
+      aValues: ChallengeGenerator.rangeToArray( 2, 10 ),
+      dValues: ChallengeGenerator.rangeToArray( 2, 10 ),
+
+      // {number|null} The level number to appear in debugging output, defaults to this.level.
+      // This is used Level 2 delegates to Level 1, so that debugging output identifies the challenge as Level 2.
+      debugLevel: null
+    }, options );
+
     super( 1, equalityExplorerStrings.level1Description );
+
+    // @private
+    this.debugLevel = options.debugLevel || this.level;
+
+    // @private
+    this.xValues = ChallengeGenerator.rangeToArray( -40, 40 );
+    this.aValues = options.aValues;
+    this.bValues = ChallengeGenerator.rangeToArray( -10, 10 );
+    this.cValues = ChallengeGenerator.rangeToArray( -10, 10 );
+    this.dValues = options.dValues;
 
     // @private methods for generating the 3 types of challenges
     this.challengeTypeMethods = [
@@ -82,13 +95,14 @@ class ChallengeGenerator1 extends ChallengeGenerator {
    * Let a be a random integer between [-10, 10], a !== [0, 1]
    * Let c = a*x, c !== 0
    *
+   * @param {number} [a] - if you'd like to use a specific value of a, otherwise randomly selected
    * @returns {Challenge}
-   * @private
+   * @protected
    */
-  nextType1() {
+  nextType1( a ) {
 
-    const x = this.randomX( X_VALUES );
-    const a = ChallengeGenerator.randomValue( A_VALUES, [ 0, 1 ] );
+    const x = this.randomX( this.xValues );
+    a = ( a !== undefined ) ? a : ChallengeGenerator.randomValue( this.aValues, [ 0, 1 ] );
     const c = a * x;
 
     // Verify that computations meeting design requirements.
@@ -97,7 +111,7 @@ class ChallengeGenerator1 extends ChallengeGenerator {
     assert && assert( c !== 0, 'c is 0' );
 
     // derivation that corresponds to design doc, displayed with 'showAnswers' query parameter
-    const debugDerivation = StringUtils.fillIn( PATTERN1, { x: x, a: a, c: c } );
+    const debugDerivation = StringUtils.fillIn( PATTERN1, { level: this.debugLevel, x: x, a: a, c: c } );
 
     // ax + 0 = 0x + c
     return new Challenge( x,
@@ -119,8 +133,8 @@ class ChallengeGenerator1 extends ChallengeGenerator {
    */
   nextType2() {
 
-    const x = this.randomX( X_VALUES );
-    const b = ChallengeGenerator.randomValue( B_VALUES, [ 0 ] );
+    const x = this.randomX( this.xValues );
+    const b = ChallengeGenerator.randomValue( this.bValues, [ 0 ] );
     const c = x + b;
 
     // Verify that computations meeting design requirements.
@@ -128,7 +142,7 @@ class ChallengeGenerator1 extends ChallengeGenerator {
     assert && assert( b !== 0, 'b is 0' );
 
     // derivation that corresponds to design doc, displayed with 'showAnswers' query parameter
-    const debugDerivation = StringUtils.fillIn( PATTERN2, { x: x, b: b, c: c } );
+    const debugDerivation = StringUtils.fillIn( PATTERN2, { level: this.debugLevel, x: x, b: b, c: c } );
 
     // 1x + b = 0x + c
     return new Challenge( x,
@@ -156,8 +170,8 @@ class ChallengeGenerator1 extends ChallengeGenerator {
     let attempts = 0; // to prevent an improbable infinite while loop
     while ( x === this.xPrevious && attempts < MAX_ATTEMPTS ) {
       attempts++;
-      c = ChallengeGenerator.randomValue( C_VALUES, [ 0 ] );
-      d = ChallengeGenerator.randomValue( D_VALUES, [ 0, 1 ] );
+      c = ChallengeGenerator.randomValue( this.cValues, [ 0 ] );
+      d = ( d !== undefined ) ? d : ChallengeGenerator.randomValue( this.dValues, [ 0, 1 ] );
       x = c * d;
     }
 
@@ -170,7 +184,7 @@ class ChallengeGenerator1 extends ChallengeGenerator {
     this.xPrevious = x;
 
     // derivation that corresponds to design doc, displayed with 'showAnswers' query parameter
-    const debugDerivation = StringUtils.fillIn( PATTERN3, { x: x, c: c, d: d } );
+    const debugDerivation = StringUtils.fillIn( PATTERN3, { level: this.debugLevel, x: x, c: c, d: d } );
 
     // (1/d)x + 0 = 0x + c
     return new Challenge( x,
--- js/solveit/model/ChallengeGenerator2.js
+++ js/solveit/model/ChallengeGenerator2.js
@@ -8,44 +8,26 @@
  */
 
 import dotRandom from '../../../../dot/js/dotRandom.js';
-import Fraction from '../../../../phetcommon/js/model/Fraction.js';
-import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import equalityExplorer from '../../equalityExplorer.js';
 import equalityExplorerStrings from '../../equalityExplorerStrings.js';
-import Challenge from './Challenge.js';
 import ChallengeGenerator from './ChallengeGenerator.js';
-
-// strings (debug)
-const PATTERN1 = 'level 2, type 1, ax = c<br>' +
-                 'x = {{x}}<br>' +
-                 'a = {{a}}<br>' +
-                 'c = a * x = {{c}}';
-const PATTERN2 = 'level 2, type 2, x + b = c<br>' +
-                 'x = {{x}}<br>' +
-                 'b = {{b}}<br>' +
-                 'c = x + b = {{c}}';
-const PATTERN3 = 'level 2, type 3, x/d = c<br>' +
-                 'c = {{c}}<br>' +
-                 'd = {{d}}<br>' +
-                 'x = c * d = {{x}} ';
-
-// constants
-const X_VALUES = ChallengeGenerator.rangeToArray( -40, 40 );
-const A_VALUES = ChallengeGenerator.rangeToArray( -10, 10 );
-const B_VALUES = ChallengeGenerator.rangeToArray( -10, 10 );
-const C_VALUES = ChallengeGenerator.rangeToArray( -10, 10 );
-const D_VALUES = ChallengeGenerator.rangeToArray( -10, 10 );
-const MAX_ATTEMPTS = 50; // max attempts in a while loop
+import ChallengeGenerator1 from './ChallengeGenerator1.js';
 
 class ChallengeGenerator2 extends ChallengeGenerator {
 
   constructor() {
     super( 2, equalityExplorerStrings.level2Description );
 
-    // @private methods for generating the 3 types of challenges
+    // @private ChallengeGenerator2 is a variation and subset of ChallengeGenerator1, so we'll use composition
+    this.challengeGenerator1 = new ChallengeGenerator1( {
+      aValues: ChallengeGenerator.rangeToArray( -10, -1 ),
+      dValues: ChallengeGenerator.rangeToArray( -10, -1 ),
+      debugLevel: this.level
+    } );
+
+    // @private methods for generating the 2 types of challenges
     this.challengeTypeMethods = [
       this.nextType1.bind( this ),
-      this.nextType2.bind( this ),
       this.nextType3.bind( this )
     ];
   }
@@ -57,121 +39,36 @@ class ChallengeGenerator2 extends ChallengeGenerator {
    * @override
    */
   nextChallengeProtected() {
-    if ( this.numberOfChallenges < 3 ) {
 
-      // For the first 3 challenges, generate 1 of each type, in order.
-      return this.challengeTypeMethods[ this.numberOfChallenges ]();
+    if ( this.numberOfChallenges === 0 ) {
+
+      // First challenge should be type 1, with a=-1.
+      return this.nextType1( -1 );
     }
     else {
 
-      // After the first 3 challenges, randomly select the challenge type.
+      // Randomly select the challenge type.
       return dotRandom.sample( this.challengeTypeMethods )();
     }
   }
 
   /**
-   * Generates the next 'type 1' challenge.
-   *
-   * Form: ax = c
-   * Let x be a random integer between [-40,40], x !== 0
-   * Let a be a random integer between [-10, 10], a !== [0, 1]
-   * Let c = a*x, c !== 0
-   *
+   * Generates the next 'type 1' challenge. Delegates to ChallengeGenerator1.
+   * @param {number} [a] - if you'd like to use a specific value of a, otherwise randomly selected
    * @returns {Challenge}
    * @private
    */
-  nextType1() {
-
-    const x = this.randomX( X_VALUES );
-    const a = ChallengeGenerator.randomValue( A_VALUES, [ 0, 1 ] );
-    const c = a * x;
-
-    // Verify that computations meeting design requirements.
-    assert && assert( x !== 0, 'x is 0' );
-    assert && assert( a !== 0, 'a is 0' );
-    assert && assert( c !== 0, 'c is 0' );
-
-    // derivation that corresponds to design doc, displayed with 'showAnswers' query parameter
-    const debugDerivation = StringUtils.fillIn( PATTERN1, { x: x, a: a, c: c } );
-
-    // ax + 0 = 0x + c
-    return new Challenge( x,
-      Fraction.fromInteger( a ), Fraction.fromInteger( 0 ),
-      Fraction.fromInteger( 0 ), Fraction.fromInteger( c ),
-      debugDerivation );
+  nextType1( a ) {
+    return this.challengeGenerator1.nextType1( a );
   }
 
   /**
-   * Generates the next 'type 2' challenge.
-   *
-   * Form: x + b = c
-   * Let x be a random integer between [-40,40], x !== 0
-   * Let b be a random integer between [-10, 10], b !== 0
-   * Let c = x + b, c == 0 is OK
-   *
-   * @returns {Challenge}
-   * @private
-   */
-  nextType2() {
-
-    const x = this.randomX( X_VALUES );
-    const b = ChallengeGenerator.randomValue( B_VALUES, [ 0 ] );
-    const c = x + b;
-
-    // Verify that computations meeting design requirements.
-    assert && assert( x !== 0, 'x is 0' );
-    assert && assert( b !== 0, 'b is 0' );
-
-    // derivation that corresponds to design doc, displayed with 'showAnswers' query parameter
-    const debugDerivation = StringUtils.fillIn( PATTERN2, { x: x, b: b, c: c } );
-
-    // 1x + b = 0x + c
-    return new Challenge( x,
-      Fraction.fromInteger( 1 ), Fraction.fromInteger( b ),
-      Fraction.fromInteger( 0 ), Fraction.fromInteger( c ),
-      debugDerivation );
-  }
-
-  /**
-   * Generates the next 'type 3' challenge.
-   *
-   * Form: x/d = c
-   * Let c be a random integer between [-10,10], c !== 0
-   * Let d be a random integer between [-10, 10], d !== [0, 1]
-   * Let x = c * d, x !== 0
-   *
+   * Generates the next 'type 3' challenge. Delegates to ChallengeGenerator1.
    * @returns {Challenge}
    * @private
    */
   nextType3() {
-
-    let x = this.xPrevious;
-    let c;
-    let d;
-    let attempts = 0; // to prevent an improbable infinite while loop
-    while ( x === this.xPrevious && attempts < MAX_ATTEMPTS ) {
-      attempts++;
-      c = ChallengeGenerator.randomValue( C_VALUES, [ 0 ] );
-      d = ChallengeGenerator.randomValue( D_VALUES, [ 0, 1 ] );
-      x = c * d;
-    }
-
-    // Verify that computations meeting design requirements.
-    assert && assert( c !== 0, 'c is 0' );
-    assert && assert( d !== 0, 'd is 0' );
-    assert && assert( d !== 1, 'd is 1' );
-    assert && assert( x !== 0, 'x is 0' );
-    assert && assert( x !== this.xPrevious || attempts === MAX_ATTEMPTS, `x === xPrevious: ${x}` );
-    this.xPrevious = x;
-
-    // derivation that corresponds to design doc, displayed with 'showAnswers' query parameter
-    const debugDerivation = StringUtils.fillIn( PATTERN3, { x: x, c: c, d: d } );
-
-    // (1/d)x + 0 = 0x + c
-    return new Challenge( x,
-      new Fraction( 1, d ), Fraction.fromInteger( 0 ),
-      Fraction.fromInteger( 0 ), Fraction.fromInteger( c ),
-      debugDerivation );
+    return this.challengeGenerator1.nextType3();
   }
 }
 
