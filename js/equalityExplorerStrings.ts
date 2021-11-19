e9568a90ed9b243f5d023ef661c6c64cedd76072
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:43 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/equalityExplorerStrings.ts
@@ -0,0 +1,45 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import equalityExplorer from './equalityExplorer.js';
+
+type StringsType = {
+  'equality-explorer': {
+    'title': string
+  },
+  'screen': {
+    'basics': string,
+    'numbers': string,
+    'variables': string,
+    'operations': string,
+    'solveIt': string
+  },
+  'snapshots': string,
+  'equationOrInequality': string,
+  'variable': string,
+  'variables': string,
+  'x': string,
+  'questionMark': string,
+  'chooseYourLevel': string,
+  'solveFor': string,
+  'levels': string,
+  'level1Description': string,
+  'level2Description': string,
+  'level3Description': string,
+  'level4Description': string,
+  'level5Description': string,
+  'next': string,
+  'numberTooBig': string,
+  'leftSideFull': string,
+  'rightSideFull': string
+};
+
+const equalityExplorerStrings = getStringModule( 'EQUALITY_EXPLORER' ) as StringsType;
+
+equalityExplorer.register( 'equalityExplorerStrings', equalityExplorerStrings );
+
+export default equalityExplorerStrings;
