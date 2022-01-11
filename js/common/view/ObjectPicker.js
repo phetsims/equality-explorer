71972098d4019fe3b9e1a789425d817eafd128f2
zepumph <michael.kauzmann@colorado.edu>
2022-01-11 16:19:11 -0700

    EnumerationProperty -> EnumerationDeprecatedProperty, https://github.com/phetsims/phet-core/issues/95

--- js/common/view/ObjectPicker.js
+++ js/common/view/ObjectPicker.js
@@ -8,7 +8,7 @@
  */
 
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
-import EnumerationProperty from '../../../../axon/js/EnumerationProperty.js';
+import EnumerationDeprecatedProperty from '../../../../axon/js/EnumerationDeprecatedProperty.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
 import Property from '../../../../axon/js/Property.js';
 import Dimension2 from '../../../../dot/js/Dimension2.js';
@@ -215,8 +215,8 @@ class ObjectPicker extends Node {
       range: new Range( 0, items.length - 1 )
     } );
 
-    const incrementButtonStateProperty = new EnumerationProperty( ButtonState, ButtonState.UP );
-    const decrementButtonStateProperty = new EnumerationProperty( ButtonState, ButtonState.UP );
+    const incrementButtonStateProperty = new EnumerationDeprecatedProperty( ButtonState, ButtonState.UP );
+    const decrementButtonStateProperty = new EnumerationDeprecatedProperty( ButtonState, ButtonState.UP );
 
     // enables the increment button
     if ( !options.incrementEnabledProperty ) {
@@ -352,7 +352,7 @@ class ObjectPicker extends Node {
 class ObjectPickerInputListener extends FireListener {
 
   /**
-   * @param {EnumerationProperty.<ButtonState>} buttonStateProperty
+   * @param {EnumerationDeprecatedProperty.<ButtonState>} buttonStateProperty
    * @param {Object} [options]
    */
   constructor( buttonStateProperty, options ) {
