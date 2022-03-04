8d9238de74da33665d503a06fe2df3282633d2be
zepumph <michael.kauzmann@colorado.edu>
2022-03-04 13:08:11 -0700

    import kite files from imports.ts, https://github.com/phetsims/kite/issues/96

--- js/common/view/ObjectPicker.js
+++ js/common/view/ObjectPicker.js
@@ -13,7 +13,7 @@ import NumberProperty from '../../../../axon/js/NumberProperty.js';
 import Property from '../../../../axon/js/Property.js';
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import Range from '../../../../dot/js/Range.js';
-import Shape from '../../../../kite/js/Shape.js';
+import { Shape } from '../../../../kite/js/imports.js';
 import EnumerationDeprecated from '../../../../phet-core/js/EnumerationDeprecated.js';
 import merge from '../../../../phet-core/js/merge.js';
 import { FireListener } from '../../../../scenery/js/imports.js';
