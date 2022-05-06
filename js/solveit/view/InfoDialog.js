1db8a2b4f581bcedfa8f7a2c7b2587b830c80c65
Chris Malley <cmalley@pixelzoom.com>
2022-05-05 21:41:50 -0600

    show level descriptions in Info dialog based on ?gameLevels, https://github.com/phetsims/equality-explorer/issues/184

--- js/solveit/view/InfoDialog.js
+++ js/solveit/view/InfoDialog.js
@@ -12,12 +12,13 @@ import { RichText } from '../../../../scenery/js/imports.js';
 import { Text } from '../../../../scenery/js/imports.js';
 import { VBox } from '../../../../scenery/js/imports.js';
 import Dialog from '../../../../sun/js/Dialog.js';
+import EqualityExplorerQueryParameters from '../../common/EqualityExplorerQueryParameters.js';
 import equalityExplorer from '../../equalityExplorer.js';
 import equalityExplorerStrings from '../../equalityExplorerStrings.js';
 
 // constants
 const TITLE_FONT = new PhetFont( 32 );
-const DESCRIPTION_FONT = new PhetFont( 24 );
+const LEVEL_INFO_FONT = new PhetFont( 24 );
 const MAX_CONTENT_WIDTH = 600;
 
 class InfoDialog extends Dialog {
@@ -27,18 +28,23 @@ class InfoDialog extends Dialog {
    */
   constructor( challengeGenerators ) {
 
-    const children = [];
-    challengeGenerators.forEach( challengeGenerators =>
-      children.push( new RichText( challengeGenerators.description, {
-        font: DESCRIPTION_FONT
-      } ) )
+    const children = challengeGenerators.map( challengeGenerator =>
+      new LevelInfoText( challengeGenerator.level, challengeGenerator.description )
     );
+
+    // Hide info for levels that are not included in gameLevels query parameter.
+    // We must still create these Nodes so that we don't risk changing the PhET-iO API.
+    if ( EqualityExplorerQueryParameters.gameLevels ) {
+      children.forEach( node => {
+        node.visible = EqualityExplorerQueryParameters.gameLevels.includes( node.level );
+      } );
+    }
 
     const content = new VBox( {
       align: 'left',
       spacing: 20,
       children: children,
-      maxWidth: MAX_CONTENT_WIDTH // scale all of the descriptions uniformly
+      maxWidth: MAX_CONTENT_WIDTH // scale all descriptions uniformly
     } );
 
     const titleNode = new Text( equalityExplorerStrings.levels, {
@@ -51,6 +57,21 @@ class InfoDialog extends Dialog {
       ySpacing: 20,
       bottomMargin: 20
     } );
+  }
+}
+
+// The info description for a level
+class LevelInfoText extends RichText {
+
+  /**
+   * @param {number} level
+   * @param {string} description
+   */
+  constructor( level, description ) {
+    super( description, {
+      font: LEVEL_INFO_FONT
+    } );
+    this.level = level; // @public
   }
 }
 
