84fc4fcd5abd2e9138c085d3fd56ca623c8e07b7
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:37 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/equalityExplorerStrings.ts
+++ js/equalityExplorerStrings.ts
@@ -5,37 +5,62 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import equalityExplorer from './equalityExplorer.js';
 
 type StringsType = {
   'equality-explorer': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'basics': string;
+    'basicsProperty': TReadOnlyProperty<string>;
     'numbers': string;
+    'numbersProperty': TReadOnlyProperty<string>;
     'variables': string;
+    'variablesProperty': TReadOnlyProperty<string>;
     'operations': string;
+    'operationsProperty': TReadOnlyProperty<string>;
     'solveIt': string;
+    'solveItProperty': TReadOnlyProperty<string>;
   };
   'snapshots': string;
+  'snapshotsProperty': TReadOnlyProperty<string>;
   'equationOrInequality': string;
+  'equationOrInequalityProperty': TReadOnlyProperty<string>;
   'variable': string;
+  'variableProperty': TReadOnlyProperty<string>;
   'variables': string;
+  'variablesProperty': TReadOnlyProperty<string>;
   'x': string;
+  'xProperty': TReadOnlyProperty<string>;
   'questionMark': string;
+  'questionMarkProperty': TReadOnlyProperty<string>;
   'chooseYourLevel': string;
+  'chooseYourLevelProperty': TReadOnlyProperty<string>;
   'solveFor': string;
+  'solveForProperty': TReadOnlyProperty<string>;
   'levels': string;
+  'levelsProperty': TReadOnlyProperty<string>;
   'level1Description': string;
+  'level1DescriptionProperty': TReadOnlyProperty<string>;
   'level2Description': string;
+  'level2DescriptionProperty': TReadOnlyProperty<string>;
   'level3Description': string;
+  'level3DescriptionProperty': TReadOnlyProperty<string>;
   'level4Description': string;
+  'level4DescriptionProperty': TReadOnlyProperty<string>;
   'level5Description': string;
+  'level5DescriptionProperty': TReadOnlyProperty<string>;
   'next': string;
+  'nextProperty': TReadOnlyProperty<string>;
   'numberTooBig': string;
+  'numberTooBigProperty': TReadOnlyProperty<string>;
   'leftSideFull': string;
+  'leftSideFullProperty': TReadOnlyProperty<string>;
   'rightSideFull': string;
+  'rightSideFullProperty': TReadOnlyProperty<string>;
 };
 
 const equalityExplorerStrings = getStringModule( 'EQUALITY_EXPLORER' ) as StringsType;
