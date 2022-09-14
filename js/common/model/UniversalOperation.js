4703607314fc443537fd1b2b9e0706100df4476a
pixelzoom <cmalley@pixelzoom.com>
2022-09-14 16:41:11 -0600

    // @ts-nocheck

--- js/common/model/UniversalOperation.js
+++ js/common/model/UniversalOperation.js
@@ -1,5 +1,6 @@
 // Copyright 2018-2021, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * Data structure that describes a 'universal operation', a term specific to this sim,
  * an operation that will be applied to both sides of the equation.
