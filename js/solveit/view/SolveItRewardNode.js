e253110c0157199448c4e6d6b4a10590ce80f08b
pixelzoom <cmalley@pixelzoom.com>
2022-09-19 12:28:08 -0600

    // @ts-nocheck

--- js/solveit/view/SolveItRewardNode.js
+++ js/solveit/view/SolveItRewardNode.js
@@ -1,5 +1,6 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * This is the reward that's shown behind RewardDialog when the user gets to 10 stars.
  * The reward varies by level.
