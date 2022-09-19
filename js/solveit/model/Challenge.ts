e6e25e8a529c4d2503e995a42654305cb8f27efb
pixelzoom <cmalley@pixelzoom.com>
2022-09-19 11:00:17 -0600

    convert to TS, https://github.com/phetsims/equality-explorer/issues/186

--- js/solveit/model/Challenge.ts
+++ js/solveit/model/Challenge.ts
@@ -1,6 +1,5 @@
 // Copyright 2018-2021, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * A challenge, in the form of an equation involving 1 variable.
  *
@@ -18,42 +17,31 @@ export default class Challenge {
    * These letters correspond to the design specification, see
    * https://docs.google.com/document/d/1vG5U9HhcqVGMvmGGXry28PLqlNWj25lStDP2vSWgUOo
    *
-   * @param {number} x - value of the variable x
-   * @param {Fraction} a - coefficient on the left side of the equation
-   * @param {Fraction} b - constant on the left side of the equation
-   * @param {Fraction} m - coefficient on the right side of the equation
-   * @param {Fraction} n - constant on the right side of the equation
-   * @param {string} debugDerivation - derivation details provided by ChallengeGenerator, contains RichText markup
+   * @param x - value of the variable x
+   * @param a - coefficient on the left side of the equation
+   * @param b - constant on the left side of the equation
+   * @param m - coefficient on the right side of the equation
+   * @param n - constant on the right side of the equation
+   * @param debugDerivation - derivation details provided by ChallengeGenerator, contains RichText markup.
+   *    With the 'showAnswers' query parameter, this information is displayed in the sim.
+   *    This information is provided by ChallengeGenerator subtypes, contains RichText markup,
+   *    and corresponds to the challenge specification in the design document, see
+   *    https://docs.google.com/document/d/1vG5U9HhcqVGMvmGGXry28PLqlNWj25lStDP2vSWgUOo
    */
-  constructor( x, a, b, m, n, debugDerivation ) {
+  public constructor( public readonly x: number,
+                      public readonly a: Fraction,
+                      public readonly b: Fraction,
+                      public readonly m: Fraction,
+                      public readonly n: Fraction,
+                      public readonly debugDerivation: string ) {
 
     assert && assert( Number.isInteger( x ), `invalid x: ${x}` );
-    assert && assert( a instanceof Fraction && a.isReduced(), `invalid a: ${a}` );
-    assert && assert( b instanceof Fraction && b.isReduced(), `invalid b: ${b}` );
-    assert && assert( m instanceof Fraction && m.isReduced(), `invalid m: ${m}` );
-    assert && assert( n instanceof Fraction && n.isReduced(), `invalid n: ${n}` );
-
-    // @public (read-only)
-    this.x = x;
-    this.a = a;
-    this.b = b;
-    this.m = m;
-    this.n = n;
-
-    // @public (read-only) details about how the challenge was derived, for debugging.
-    // With the 'showAnswers' query parameter, this information is displayed in the sim.
-    // This information is provided by ChallengeGenerator subtypes, contains RichText markup,
-    // and corresponds to the challenge specification in the design document, see
-    // https://docs.google.com/document/d/1vG5U9HhcqVGMvmGGXry28PLqlNWj25lStDP2vSWgUOo
-    this.debugDerivation = debugDerivation;
-
-    // verify that the we have an equality
     assert && assert( a.timesInteger( x ).plus( b ).reduce().equals( m.timesInteger( x ).plus( n ).reduce() ),
-      `challenge is an inequality: ${this.toString()}` );
+      `challenge must be an equality: ${this.toString()}` );
   }
 
-  // @public (debug) do not rely on format!
-  toString() {
+  // For debugging. Do not rely on this format!
+  public toString(): string {
     return StringUtils.fillIn( '{{a}} x + {{b}} = {{m}} x + {{n}} (x={{x}})', {
       a: fractionToString( this.a ),
       b: fractionToString( this.b ),
@@ -62,20 +50,14 @@ export default class Challenge {
       x: this.x
     } );
   }
-
-  // @private
-
 }
 
 /**
- * Converts a Fraction to a string representation.
- * This is done here because Fraction.toString is in common code, and doesn't provide the format that we want.
- * @param {Fraction} f
- * @returns {string}
+ * Converts a Fraction to a string representation. This is done here because Fraction.toString is in common code,
+ * and doesn't provide the format that we want for our debugging output.
  */
-function fractionToString( f ) {
-  assert && assert( f instanceof Fraction, `invalid f: ${f}` );
-  return ( f.isInteger() ? f.getValue() : ( `${f.numerator}/${f.denominator}` ) );
+function fractionToString( f: Fraction ): string {
+  return ( f.isInteger() ? `${f.getValue()}` : `${f.numerator}/${f.denominator}` );
 }
 
 equalityExplorer.register( 'Challenge', Challenge );
