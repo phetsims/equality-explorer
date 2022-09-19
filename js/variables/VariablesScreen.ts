86072d535aaba5ce4147348c95243cb8edaadfcb
pixelzoom <cmalley@pixelzoom.com>
2022-09-19 16:47:53 -0600

    convert to TS, https://github.com/phetsims/equality-explorer/issues/186

--- js/variables/VariablesScreen.ts
+++ js/variables/VariablesScreen.ts
@@ -1,6 +1,5 @@
 // Copyright 2017-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * The 'Variables' screen.
  *
@@ -8,29 +7,31 @@
  */
 
 import Property from '../../../axon/js/Property.js';
-import merge from '../../../phet-core/js/merge.js';
+import optionize, { EmptySelfOptions } from '../../../phet-core/js/optionize.js';
+import PickRequired from '../../../phet-core/js/types/PickRequired.js';
 import EqualityExplorerColors from '../common/EqualityExplorerColors.js';
-import EqualityExplorerScreen from '../common/EqualityExplorerScreen.js';
+import EqualityExplorerScreen, { EqualityExplorerScreenOptions } from '../common/EqualityExplorerScreen.js';
 import EqualityExplorerScreenIcons from '../common/EqualityExplorerScreenIcons.js';
 import equalityExplorer from '../equalityExplorer.js';
 import EqualityExplorerStrings from '../EqualityExplorerStrings.js';
 import VariablesModel from './model/VariablesModel.js';
 import VariablesScreenView from './view/VariablesScreenView.js';
 
-export default class VariablesScreen extends EqualityExplorerScreen {
+type SelfOptions = EmptySelfOptions;
 
-  /**
-   * @param {Object} [options]
-   */
-  constructor( options ) {
+type VariablesScreenOptions = SelfOptions & PickRequired<EqualityExplorerScreenOptions, 'tandem'>;
 
-    options = merge( {
+export default class VariablesScreen extends EqualityExplorerScreen<VariablesModel, VariablesScreenView> {
 
-      // EqualityExplorerScreen options
+  public constructor( providedOptions: VariablesScreenOptions ) {
+
+    const options = optionize<VariablesScreenOptions, SelfOptions, EqualityExplorerScreenOptions>()( {
+
+      // EqualityExplorerScreenOptions
       name: EqualityExplorerStrings.screen.variablesStringProperty,
       backgroundColorProperty: new Property( EqualityExplorerColors.VARIABLES_SCREEN_BACKGROUND ),
       homeScreenIcon: EqualityExplorerScreenIcons.createVariablesScreenIcon()
-    }, options );
+    }, providedOptions );
 
     super(
       () => new VariablesModel( options.tandem.createTandem( 'model' ) ),
