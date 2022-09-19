1024c9b56bb6a6ed28623ad7a4dd28ce4cb11dc8
pixelzoom <cmalley@pixelzoom.com>
2022-09-19 13:28:21 -0600

    // @ts-nocheck

--- js/basics/model/FruitsScene.js
+++ js/basics/model/FruitsScene.js
@@ -1,5 +1,6 @@
 // Copyright 2017-2021, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * The 'Fruits' scene in the 'Basics' screen.
  *
--- js/basics/model/ShapesScene.js
+++ js/basics/model/ShapesScene.js
@@ -1,5 +1,6 @@
 // Copyright 2017-2021, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * The 'Shapes' scene in the 'Basics' screen.
  *
