f7c36dc97769c890af4f0a567eb3690159ba39c1
pixelzoom <cmalley@pixelzoom.com>
2022-09-19 12:40:12 -0600

    // @ts-nocheck

--- js/basics/view/BasicsSceneNode.js
+++ js/basics/view/BasicsSceneNode.js
@@ -1,5 +1,6 @@
 // Copyright 2017-2021, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * View of a scene in the 'Basics' screen.
  *
--- js/common/view/EqualityExplorerSceneNode.js
+++ js/common/view/EqualityExplorerSceneNode.js
@@ -1,5 +1,6 @@
 // Copyright 2017-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * Abstract base type for displaying scenes in Equality Explorer.
  *
