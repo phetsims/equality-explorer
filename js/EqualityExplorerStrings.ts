00c46ff1e724d996e4b9a73f79c1b6f5effcbb7f
pixelzoom <cmalley@pixelzoom.com>
2022-09-20 15:47:16 -0600

    "supportsDynamicLocale": true, https://github.com/phetsims/equality-explorer/issues/187

--- js/EqualityExplorerStrings.ts
+++ js/EqualityExplorerStrings.ts
@@ -10,56 +10,32 @@ import equalityExplorer from './equalityExplorer.js';
 
 type StringsType = {
   'equality-explorer': {
-    'title': string;
     'titleStringProperty': TReadOnlyProperty<string>;
   };
   'screen': {
-    'basics': string;
     'basicsStringProperty': TReadOnlyProperty<string>;
-    'numbers': string;
     'numbersStringProperty': TReadOnlyProperty<string>;
-    'variables': string;
     'variablesStringProperty': TReadOnlyProperty<string>;
-    'operations': string;
     'operationsStringProperty': TReadOnlyProperty<string>;
-    'solveIt': string;
     'solveItStringProperty': TReadOnlyProperty<string>;
   };
-  'snapshots': string;
   'snapshotsStringProperty': TReadOnlyProperty<string>;
-  'equationOrInequality': string;
   'equationOrInequalityStringProperty': TReadOnlyProperty<string>;
-  'variable': string;
   'variableStringProperty': TReadOnlyProperty<string>;
-  'variables': string;
   'variablesStringProperty': TReadOnlyProperty<string>;
-  'x': string;
   'xStringProperty': TReadOnlyProperty<string>;
-  'questionMark': string;
   'questionMarkStringProperty': TReadOnlyProperty<string>;
-  'chooseYourLevel': string;
   'chooseYourLevelStringProperty': TReadOnlyProperty<string>;
-  'solveFor': string;
   'solveForStringProperty': TReadOnlyProperty<string>;
-  'levels': string;
   'levelsStringProperty': TReadOnlyProperty<string>;
-  'level1Description': string;
   'level1DescriptionStringProperty': TReadOnlyProperty<string>;
-  'level2Description': string;
   'level2DescriptionStringProperty': TReadOnlyProperty<string>;
-  'level3Description': string;
   'level3DescriptionStringProperty': TReadOnlyProperty<string>;
-  'level4Description': string;
   'level4DescriptionStringProperty': TReadOnlyProperty<string>;
-  'level5Description': string;
   'level5DescriptionStringProperty': TReadOnlyProperty<string>;
-  'next': string;
   'nextStringProperty': TReadOnlyProperty<string>;
-  'numberTooBig': string;
   'numberTooBigStringProperty': TReadOnlyProperty<string>;
-  'leftSideFull': string;
   'leftSideFullStringProperty': TReadOnlyProperty<string>;
-  'rightSideFull': string;
   'rightSideFullStringProperty': TReadOnlyProperty<string>;
 };
 
