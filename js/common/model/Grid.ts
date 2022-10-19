a54844389438d475b122badd288ab0a727eac3c3
pixelzoom <cmalley@pixelzoom.com>
2022-10-19 10:17:28 -0400

    add Grid.dispose, https://github.com/phetsims/equality-explorer/issues/192

--- js/common/model/Grid.ts
+++ js/common/model/Grid.ts
@@ -104,6 +104,10 @@ export default class Grid {
     } );
   }
 
+  public dispose(): void {
+    assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
+  }
+
   /**
    * Gets the y coordinate of the top of the grid.
    */
