f9ec4c330d62801f70b8f9ef953294b5db4fe219
phet-dev <phetadmin@gmail.com>
2024-09-27 03:29:11 -0600

    update copyright dates from daily grunt work

--- js/common/model/ObjectTermCreator.ts
+++ js/common/model/ObjectTermCreator.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2023, University of Colorado Boulder
+// Copyright 2017-2024, University of Colorado Boulder
 
 /**
  * ObjectTermCreator creates and manages terms that are associated with real-world objects.
