786229ac4f04652c81de4db8f7dd847dd3dc39f5
pixelzoom <cmalley@pixelzoom.com>
2024-11-19 10:59:50 -0700

    fix formatting of "Solve for x", https://github.com/phetsims/equality-explorer/issues/216

--- js/common/EqualityExplorerDerivedStrings.ts
+++ js/common/EqualityExplorerDerivedStrings.ts
@@ -6,21 +6,25 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import PatternStringProperty from '../../../axon/js/PatternStringProperty.js';
 import { RichText } from '../../../scenery/js/imports.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import equalityExplorer from '../equalityExplorer.js';
 import EqualityExplorerStrings from '../EqualityExplorerStrings.js';
+import DerivedStringProperty from '../../../axon/js/DerivedStringProperty.js';
+import StringUtils from '../../../phetcommon/js/util/StringUtils.js';
+import MathSymbolFont from '../../../scenery-phet/js/MathSymbolFont.js';
 
 const DERIVED_STRINGS_TANDEM = Tandem.getDerivedStringsTandem();
 
 const EqualityExplorerDerivedStrings = {
 
-  solveForXStringProperty: new PatternStringProperty( EqualityExplorerStrings.solveForStringProperty, {
-    variable: EqualityExplorerStrings.xStringProperty
-  }, {
-    tandem: DERIVED_STRINGS_TANDEM.createTandem( RichText.STRING_PROPERTY_TANDEM_NAME )
-  } )
+  solveForXStringProperty: new DerivedStringProperty(
+    [ EqualityExplorerStrings.solveForStringProperty, EqualityExplorerStrings.xStringProperty ],
+    ( solveForString, xString ) => StringUtils.fillIn( solveForString, {
+      variable: MathSymbolFont.getRichTextMarkup( xString )
+    } ), {
+      tandem: DERIVED_STRINGS_TANDEM.createTandem( RichText.STRING_PROPERTY_TANDEM_NAME )
+    } )
 };
 
 equalityExplorer.register( 'EqualityExplorerDerivedStrings', EqualityExplorerDerivedStrings );
