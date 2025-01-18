28147b5d29cab0b69b184600a121af43f656360f
phet-dev <phetadmin@gmail.com>
2025-01-18 03:35:02 -0700

    update copyright dates from daily grunt work

--- js/common/view/SnapshotsAccordionBox.ts
+++ js/common/view/SnapshotsAccordionBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2024, University of Colorado Boulder
+// Copyright 2017-2025, University of Colorado Boulder
 
 /**
  * Accordion box that allows the student to save and restore snapshots, specific configurations of a scene.
